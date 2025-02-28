81729fd64693e08b6dec4be099bd1c9da1bb656a
Aaron Saunders <aaronksaunders@gmail.com>
2025-02-28 10:30:38 -0500

    feat: Add Payload Form Builder plugin and implement contact form

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -34,6 +35,39 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    formBuilderPlugin({
+      formOverrides: {
+        fields: ({ defaultFields }) => {
+          return [
+            ...defaultFields,
+            {
+              name: 'hasAttachment',
+              type: 'checkbox',
+            },
+            {
+              name: 'hasAttachmentLabel',
+              type: 'text',
+            },
+          ]
+        },
+      },
+      formSubmissionOverrides: {
+        fields: ({ defaultFields }) => {
+          return [
+            ...defaultFields,
+            {
+              name: 'file',
+              type: 'upload',
+              relationTo: 'media',
+              admin: {
+                allowCreate: true,
+                allowEdit: true,
+              },
+            },
+          ]
+        },
+      },
+    }),
     // storage-adapter-placeholder
   ],
 })
